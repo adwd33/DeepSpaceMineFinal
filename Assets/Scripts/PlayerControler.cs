7959275dec2f6796c133ea0e2bb64d715f92ba68
adwd33 <adwd33@mail.missouri.edu>
2014-10-22 00:30:19 -0500

    Movement Controls Finished
    Looks like we're having synchronization issues again. Why must life be
    full of suffering? In any case, on my computer at least, the ship has
    the controls I described.

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -10,7 +10,6 @@ public class Boundary
 public class PlayerControler : MonoBehaviour
 {
 	public float speed;
-	public float tilt;
 	public Boundary boundary;
 	
 	public GameObject shot;
@@ -21,127 +20,36 @@ public class PlayerControler : MonoBehaviour
 
 	public GameObject cameraRod;
 
-	// Stuff having to do with mouselook
-	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-	public RotationAxes axes = RotationAxes.MouseXAndY;
-	public float sensitivityX;
-	public float sensitivityY;
-	
-	public float minimumX = -360F;
-	public float maximumX = 360F;
-	
-	public float minimumY = -360F;
-	public float maximumY = 360F;
-
-	float rotationX = 0F;
-	float rotationY = 0F;
-	// End stuff having to do with mouselook
-	
-	//Ship posture
-	//public float lr_Rotate_MaxAngle = -90.0f;
-	//public float lr_m_Angle = 0.0f;
-	//public float m_SmoothValue = 3.0f;
-	
-	void Awake()
-	{
-		//lr_m_Angle = transform.rotation.eulerAngles.z;
-		//ht_m_Angle = transform.rotation.eulerAngles.x;
-		//lr_Rotate_MaxAngle += lr_m_Angle;
-		//ht_Rotate_MaxAngle += ht_m_Angle;
-	}
+	// Stuff having to do with upgrades
+	public float movementLevel;
+	// End stuff having to do with upgrades
 
 	void Start ()
 	{
-		// Make the rigid body not change rotation
-//		if (rigidbody)
-//			rigidbody.freezeRotation = true;
-		//originalRotation = transform.localRotation;
+		cameraRod = GameObject.Find ("cameraRod");
 	}
 	
 	void Update ()
 	{
-		// Create a vector of the difference between this transform's rotation and the cameraRod's transform's rotation
-		Vector3 vChange = new Vector3(cameraRod.transform.eulerAngles.x - transform.eulerAngles.x,
-		                           cameraRod.transform.eulerAngles.y - transform.eulerAngles.y,
-		                           cameraRod.transform.eulerAngles.z - transform.eulerAngles.z);
-
-		// Add the difference to this transform
-		Vector3 newDir = new Vector3 (transform.eulerAngles.x + vChange.x,
-		                              transform.eulerAngles.y + vChange.y,
-		                              transform.eulerAngles.z + vChange.z);
-		transform.eulerAngles = newDir;
-
-		// Subtract the different from cameraRod's transform
-		newDir = new Vector3 (cameraRod.transform.eulerAngles.x - vChange.x,
-		                      cameraRod.transform.eulerAngles.y - vChange.y,
-		                      cameraRod.transform.eulerAngles.z - vChange.z);
-		cameraRod.transform.eulerAngles = newDir;
-
-		/*
-		Vector3 swap = cameraRod.transform.eulerAngles - transform.eulerAngles;
-		transform.forward = cameraRod.transform.forward;
-		cameraRod.transform.Rotate (-swap);
-		*/
-		/*
-		rotationX = sensitivityX * Input.GetAxis ("Mouse X");
-		rotationY = sensitivityY * Input.GetAxis ("Mouse Y");
-		transform.Rotate (-rotationY, rotationX, 0, Space.Self);
-		Debug.Log("X = " + rotationX);
-		Debug.Log("Y = " + rotationY);
-		Debug.Log ("Math: " + rotationX + " * " + sensitivityX + " = " + (rotationX * sensitivityX));*/
-		/*
-		// Stuff having to do with mouselook
-		if (axes == RotationAxes.MouseXAndY)
-		{
-			//ship posture adjust
-			//float lr_Input = Input.GetAxis("Horizontal");
-			//lr_m_Angle = Mathf.Lerp(lr_m_Angle, lr_Rotate_MaxAngle * lr_Input, Time.deltaTime * m_SmoothValue);
-			//Vector3 euler = transform.rotation.eulerAngles;
-			//euler.z = lr_m_Angle;
-			//transform.localRotation = Quaternion.Euler(euler);
-			
-			//float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-			//float rotationX = transform.localEulerAngles.y + Input.GetAxis("Horizontal") * sensitivityX;
-			rotationX = Input.GetAxis("Mouse X") * sensitivityX;
-			rotationY = Input.GetAxis("Mouse Y") * sensitivityY;
-
-			//rotationX = Mathf.Clamp (rotationX, minimumX, maximumX);
-			//rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			//transform.localEulerAngles = new Vector3(-rotationY, rotationX, lr_m_Angle);
-
-			//Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
-			//Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, -Vector3.right);
-			
-			//transform.localRotation = originalRotation * xQuaternion * yQuaternion;
-			transform.Rotate (-rotationY, rotationX, 0, Space.Self);
-			Debug.Log("X = " + rotationX);
-			Debug.Log("Y = " + rotationY);
+		// Set the turning speed of the ship based on the ship's movement level
+		float turnSpeed;
+		if (movementLevel == 0) {
+			turnSpeed = 100F;
+		} else if (movementLevel == 1) {
+			turnSpeed = 200F;
+		} else if (movementLevel == 2) {
+			turnSpeed = 300F;		
+		} else {
+			turnSpeed = 700F;
 		}
-		else if (axes == RotationAxes.MouseX)
-		{
-			rotationX = Input.GetAxis("Mouse X") * sensitivityX;
-			//rotationX = Mathf.Clamp (rotationX, minimumX, maximumX);
-			
-			//Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
-			//transform.localRotation = originalRotation * xQuaternion;
-			transform.Rotate (0, rotationX, 0, Space.Self);
-			Debug.Log("X = " + rotationX);
-		}
-		else
-		{
-			rotationY = Input.GetAxis("Mouse Y") * sensitivityY;
-			//rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			//Quaternion yQuaternion = Quaternion.AngleAxis (-rotationY, Vector3.right);
-			//transform.localRotation = originalRotation * yQuaternion;
-			transform.Rotate (-rotationY, 0, 0, Space.Self);
-			Debug.Log("Y = " + rotationY);
-		}*/
-		// End stuff having to do with mouselook
+
+		// Rotate the ship towards the camera's angle based on the turning speed
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, cameraRod.transform.rotation, turnSpeed * Time.deltaTime);
 	}
 	
 	void FixedUpdate ()
 	{
+		// Shoot and control the rate of fire
 		if (Input.GetButton("Fire1") && Time.time > nextFire)
 		{
 			nextFire = Time.time + fireRate;
@@ -149,10 +57,36 @@ public class PlayerControler : MonoBehaviour
 			Instantiate(shot, shotSpawn.position, rigidbody.rotation);
 			audio.Play ();
 		}
-		
-		float moveHorizontal = Input.GetAxis ("Speed");
-		
-		// Moves the ship according to its orientation when appropriate keys are pressed
-		transform.Translate (0, 0, moveHorizontal/50 * speed, Space.Self);
+
+		// Set the movement speed based on the ship's movement level
+		float moveSpeed;
+		if (movementLevel == 0) {
+			moveSpeed = 8;
+		} else if (movementLevel == 1) {
+			moveSpeed = 10;
+		} else if (movementLevel == 2) {
+			moveSpeed = 12;		
+		} else {
+			moveSpeed = 15;
+		}
+
+		// Reduce the movement speed for more controlled movement if the button is being held down
+		if (Input.GetButton ("Slow"))
+			moveSpeed /= 2;
+
+		// Move the player in the direction of any buttons being pressed
+		if (Input.GetButton("Forward"))
+			rigidbody.AddRelativeForce(Vector3.forward * moveSpeed);
+		if (Input.GetButton("Back"))
+			rigidbody.AddRelativeForce(Vector3.back * moveSpeed);
+		if (Input.GetButton("Left"))
+			rigidbody.AddRelativeForce(Vector3.left * moveSpeed);
+		if (Input.GetButton("Right"))
+			rigidbody.AddRelativeForce(Vector3.right * moveSpeed);
+		if (Input.GetButton("Up"))
+			rigidbody.AddRelativeForce(Vector3.up * moveSpeed);
+		if (Input.GetButton("Down"))
+			rigidbody.AddRelativeForce(Vector3.down * moveSpeed);
+
 	}
 }
