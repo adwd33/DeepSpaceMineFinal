a1374a8828399030b636fb0f7e445baff0ad207c
mizzouman <krjt28@mail.missouri.edu>
2014-10-30 10:57:26 -0500

    Turrets show up
    When you click on the slot the turret shows up

---
+++ Assets/Scripts/turrets/T2SVisible.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class T2SVisible : MonoBehaviour {
+
+	public GameObject menu;
+	// Use this for initialization
+	void Start () {
+		renderer.enabled = !renderer.enabled;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (menu.GetComponent<HomeBaseMenu> ().turret2 == true) {
+			renderer.enabled = true;
+		}
+	}
+}
