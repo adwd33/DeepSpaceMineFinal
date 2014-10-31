87b11a28509c2cb87c6683e31b5d86152018561a
tzfyb <tzfyb@mail.missouri.edu>
2014-10-31 16:27:19 -0500

    Control&Camera Switch and PlayerCenter
    Press '9' to switch between Aaron's control system and mine (including
    camera).
    
    Also now PlayerCenter.cs were added to maintain all the action about
    player, which means now PlayerControler and PlayerControllerTest are
    only responsible for the ship control part. For example, the
    getResourceList method should now be called through PlayerCenter. And
    latter the player health will also be added into PlayerCenter.

---
+++ Assets/Scripts/Player/PlayerCenter.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCenter : MonoBehaviour {
+
+	//for switch between two control
+	public GameObject cameraRod;
+	public GameObject MainCamera;
+
+	//resources record
+	public int[] resources = new int[11];
+
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<PlayerControler>().enabled = false;
+		gameObject.GetComponent<PlayerControllerTest>().enabled = true;
+
+		cameraRod = GameObject.Find ("Player2/cameraRod");
+		MainCamera = GameObject.Find("Player2/MainCamera");
+
+		cameraRod.SetActive (false);
+		MainCamera.SetActive (true);
+
+		//Initialize the resource record
+		resources = new int[11] {0, 0, 0, 0,
+			0, 0, 0, 0,
+			0, 0, 0};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Switch")) {
+			gameObject.GetComponent<PlayerControler>().enabled = !gameObject.GetComponent<PlayerControler>().enabled;
+			gameObject.GetComponent<PlayerControllerTest>().enabled = !gameObject.GetComponent<PlayerControllerTest>().enabled;
+			cameraRod.SetActive (!cameraRod.activeSelf);
+			MainCamera.SetActive (!MainCamera.activeSelf);
+		}
+	}
+
+	public void resourceCollector(int type, int amount)
+	{
+		//Debug.Log ("Be Called");
+		Debug.Log (type + " " + amount);
+		resources [type] += amount;
+	}
+	
+	public int[] getResourceList()
+	{
+		return resources;
+	}
+}
