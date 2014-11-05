3aa4bbfe9d2f45fd42582bf2d7cc078eeb49d418
mizzouman <krjt28@mail.missouri.edu>
2014-11-05 11:06:44 -0600

    Bullet direction
    Bullet now initally goes to you but then drops down (and no gravity is
    not selected for it) will figure it out in a couple of hours

--- Assets/Scripts/turrets/Turret1Control.js
+++ Assets/Scripts/turrets/Turret1Control.js
@@ -9,21 +9,4 @@ function Start () {
 function Update () {
 	transform.LookAt(LookAtTarget);
 	
-	var seconds : int = Time.time;
-	var oddeven = (seconds % 2);
-	
-	if(oddeven)
-		shoot(seconds);
-}
-
-function shoot(seconds){
-
-	if(seconds != savedtime)
-	{
-		var bullet = Instantiate(bulletPreFab, transform.Find("turret1shot").transform.position, Quaternion.identity);
-		
-		bullet.rigidbody.AddForce(transform.forward*10);
-		
-		savedtime = seconds;
-	}
 }
---
+++ Assets/T1BarrelShot.js
@@ -0,0 +1,27 @@
+﻿var bulletPreFab:Transform;
+var savedtime;
+
+function Start () {
+
+}
+
+function Update () {
+
+	var seconds : int = Time.time;
+	var oddeven = (seconds % 2);
+	
+	if(oddeven)
+		shoot(seconds);
+}
+
+function shoot(seconds){
+
+	if(seconds != savedtime)
+	{
+		var bullet = Instantiate(bulletPreFab, transform.Find("turret1shot").transform.position, Quaternion.identity);
+		
+		bullet.rigidbody.AddForce(transform.forward*10);
+		
+		savedtime = seconds;
+	}
+}
