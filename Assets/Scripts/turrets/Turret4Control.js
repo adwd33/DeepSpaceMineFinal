7257d51395db6bb7f9353d973a8c9f878d3cc972
mizzouman <krjt28@mail.missouri.edu>
2014-11-06 12:30:33 -0600

    All turrets work
    All turrets aim/fire only if they have line of sight and they all aim
    correctly except just above the target a little bit

--- Assets/Scripts/turrets/Turret4Control.js
+++ Assets/Scripts/turrets/Turret4Control.js
@@ -1,5 +1,6 @@
 ﻿var LookAtTarget:Transform;
 var savedtime;
+var bulletPreFab:Transform;
 
 function Start () {
 
@@ -7,6 +8,26 @@ function Start () {
 
 function Update () {
 	if(GameObject.Find("vehicle_playerShip").transform.position.x <= 0)
+	{
 		transform.LookAt(LookAtTarget);
 	
+		var seconds : int = Time.time;
+		var oddeven = (seconds % 2);
+		
+		if(oddeven)
+			shoot(seconds);
+		
+	}
+}
+
+function shoot(seconds){
+
+	if(seconds != savedtime)
+	{
+		var bullet = Instantiate(bulletPreFab, transform.Find("spawnpoint").transform.position, Quaternion.identity);
+		
+		bullet.rigidbody.AddForce(transform.forward*50);
+		
+		savedtime = seconds;
+	}
 }
