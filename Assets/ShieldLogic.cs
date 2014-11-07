e179fc8db8ee262aeb4ebc059605abce017abdf0
mizzouman <krjt28@mail.missouri.edu>
2014-11-06 19:23:17 -0600

    Base Shield
    Very primitive shield but there is one now around the base

---
+++ Assets/ShieldLogic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldLogic : MonoBehaviour {
+
+	public int shieldStrength = 10;
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "boundary" || other.tag == "TurretShot")
+		{
+			return;
+		}
+		if (shieldStrength > 0) {
+			shieldStrength--;
+			Destroy(other.gameObject);
+			return;
+		}
+		Destroy(gameObject);
+	}
+}
