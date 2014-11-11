6d7e42c15b65e2cb920ef6dfe3238ced0ed35630
kpwf48 <kpwf48@mail.missouri.edu>
2014-11-11 10:36:09 -0600

    Revert "The Big Bad Guy Commit"
    This reverts commit 24364fb64a96628d75f86c0fc38d61ee5648587f.

--- Assets/Scripts/Weapon/Weapon_0_Ctrl.cs
+++ Assets/Scripts/Weapon/Weapon_0_Ctrl.cs
@@ -9,10 +9,6 @@ public class Weapon_0_Ctrl : MonoBehaviour {
 	public GameObject boom;
 	public float lifetime;
 
-	public GameController gc;
-	public GameObject PlayerWorld;
-
-
 //	void Awake()
 //	{
 //		Destroy (gameObject, lifetime);
@@ -21,9 +17,6 @@ public class Weapon_0_Ctrl : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		shotSpawn = GameObject.Find("Player2/Ship/ShotSpawn");
-
-		PlayerWorld = GameObject.Find("Player2/PlayerWorld");
-		gc = PlayerWorld.GetComponent<GameController>();
 
 		rigidbody.velocity = transform.forward * (speed + PlayerControllerTest.speed);
 		Destroy (gameObject, lifetime);
@@ -42,20 +35,13 @@ public class Weapon_0_Ctrl : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if(other.gameObject.tag != "ShipBody" && other.gameObject.tag != "Player" && other.gameObject.tag != "package" && other.gameObject.tag != "Enemy")
+		if(other.gameObject.tag != "ShipBody" && other.gameObject.tag != "Player" && other.gameObject.tag != "package")
 		{
 			Debug.Log (other.name);
 			other.rigidbody.AddForce(transform.forward * 200);
 			other.transform.SendMessage("ApplyDamage",Damage,SendMessageOptions.DontRequireReceiver);
 			Destroy(gameObject);
 			Instantiate(boom,gameObject.transform.position-gameObject.transform.forward * 1,Quaternion.identity);
-		} else if(other.gameObject.tag == "Enemy")
-		{
-			Debug.Log (other.name);
-			other.rigidbody.AddForce(transform.forward * 200);
-			other.transform.SendMessage("ApplyDamage",Damage,SendMessageOptions.DontRequireReceiver);
-			Instantiate(boom,gameObject.transform.position-gameObject.transform.forward * 1,Quaternion.identity);
-			gc.SpawnRemoved(other.gameObject);
 		}
 
 	}
