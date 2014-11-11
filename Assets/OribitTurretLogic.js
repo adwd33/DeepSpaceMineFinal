d1cf15d44a045b11ed646c4a77154cceb586869d
mizzouman <krjt28@mail.missouri.edu>
2014-11-11 12:52:35 -0600

    Orbiting Turret
    I figured out the logic to have orbiting turrets

---
+++ Assets/OribitTurretLogic.js
@@ -0,0 +1,98 @@
+﻿var LookAtTarget:Transform;
+var savedtime;
+var bulletPreFab:Transform;
+var misslePreFab:Transform;
+var bombPreFab:Transform;
+var bomb:int;
+var missle:int;
+var turret:int;
+public var center : Transform;
+public var axis   : Vector3 = Vector3.up;
+public var radius = 2.0;
+public var radiusSpeed = 0.5;
+public var rotationSpeed = 80.0; 
+
+var bombtime = 0;
+
+function Start () {
+
+	bomb = 0;
+	missle = 0;
+	turret = 1;
+}
+
+function Update () {
+
+	transform.RotateAround (center.position, axis, rotationSpeed * Time.deltaTime);
+	transform.LookAt(LookAtTarget);
+	
+	var seconds : int = Time.time;
+	var oddeven;
+	
+	if(bomb)
+		oddeven = (seconds % 10);
+	else 
+		oddeven = (seconds % 2);
+	
+	if(oddeven)
+		shoot(seconds);
+	
+	
+	if (Input.GetButtonDown("Fire2"))
+	{
+		bomb = 1;
+		turret = 0;
+		missle = 0;
+	}
+	if (Input.GetButtonDown("Fire3"))
+	{
+		bomb = 0;
+		turret = 0;
+		missle = 1;
+	}
+		if (Input.GetButtonDown("Fire4"))
+	{
+		bomb = 0;
+		turret = 1;
+		missle = 0;
+	}
+	
+	 bombtime++;
+}
+
+function shoot(seconds){
+
+	if(turret == 1)
+	{
+		if(seconds != savedtime)
+		{
+			var bullet = Instantiate(bulletPreFab, transform.Find("spawnpoint").transform.position, Quaternion.identity);
+			
+			bullet.rigidbody.AddForce(transform.forward*2000);
+			
+			savedtime = seconds;
+		}
+	}
+	else if(missle == 1)
+	{
+		var missle = Instantiate(misslePreFab, transform.Find("spawnpoint").transform.position, Quaternion.identity);
+			
+		missle.rigidbody.AddForce(transform.forward*200);
+	}
+	else
+	{
+		if(bombtime > 240)
+		{
+			if(seconds != savedtime)
+			{
+				
+				var bomb = Instantiate(bombPreFab, transform.Find("spawnpoint").transform.position, Quaternion.identity);
+				
+				bomb.rigidbody.AddForce(transform.forward*1000);
+				
+				savedtime = seconds;
+			}
+			bombtime = 0;
+		}
+	}
+}
