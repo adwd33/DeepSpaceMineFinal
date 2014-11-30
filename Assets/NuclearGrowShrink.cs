4aac0c67df5b6c819ee76fb13b00cd31a3ce2f3e
mizzouman <krjt28@mail.missouri.edu>
2014-11-30 08:58:02 -0600

    nuclear explosion fix
    it now grows to a scale of 8.5 and shrinks to 0 and when it hits 0 it
    gets destroyed

--- Assets/NuclearGrowShrink.cs
+++ Assets/NuclearGrowShrink.cs
@@ -4,9 +4,19 @@ using System.Collections;
 public class NuclearGrowShrink : MonoBehaviour {
 
 	private Vector3 max = new Vector3(8.5F, 8.5F, 8.5F);
+	private bool reach = false;
+	private float min = 8.5F;
 
 	void Update() {
-		if(transform.localScale.sqrMagnitude < max.sqrMagnitude)
-			transform.localScale += new Vector3(0.025F, 0.025F, 0.025F);
+		if (transform.localScale.sqrMagnitude < max.sqrMagnitude && !reach) {
+				transform.localScale += new Vector3 (0.025F, 0.025F, 0.025F);
+		} else if (min > 0) {
+				reach = true;
+				transform.localScale -= new Vector3 (0.02F, 0.02F, 0.02F);
+			min -= 0.02F;
+		} else {
+			Destroy(gameObject);
+		}
+
 	}
 }
