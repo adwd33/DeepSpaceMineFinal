da0052dcd5650a0228b1da5eae7d696e9ee40c9e
mizzouman <krjt28@mail.missouri.edu>
2014-11-29 23:38:44 -0600

    missile and slug
    logic for the ammo updated

---
+++ Assets/misslelogic.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+//only destroys the enemys
+		if(other.tag == "enemy"){
+			Destroy(other.gameObject);
+			Destroy (gameObject);
+			globalMethod.enemys -= 1;
+		}
