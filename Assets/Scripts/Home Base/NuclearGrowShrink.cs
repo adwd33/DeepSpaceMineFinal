b946c0c441d42792624e5392b12f9db0243cbe2a
mizzouman <krjt28@mail.missouri.edu>
2014-12-02 08:27:55 -0600

    explosion
    files for explosions of the enemys

--- Assets/Scripts/Home Base/NuclearGrowShrink.cs
+++ Assets/Scripts/Home Base/NuclearGrowShrink.cs
@@ -3,20 +3,23 @@ using System.Collections;
 
 public class NuclearGrowShrink : MonoBehaviour {
 
-	private Vector3 max = new Vector3(8.5F, 8.5F, 8.5F);
+	private Vector3 max = new Vector3(13F, 13F, 13F);
 	private bool reach = false;
 	private float min = 8.5F;
 
 	void Update() {
 		if (transform.localScale.sqrMagnitude < max.sqrMagnitude && !reach) {
-				transform.localScale += new Vector3 (0.025F, 0.025F, 0.025F);
+				transform.localScale += new Vector3 (0.04F, 0.04F, 0.04F);
 		} else if (min > 0) {
 				reach = true;
 				transform.localScale -= new Vector3 (0.02F, 0.02F, 0.02F);
-			min -= 0.02F;
+			min -= 0.03F;
 		} else {
 			Destroy(gameObject);
 		}
-
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "enemy")
+			Destroy (other.gameObject);
 	}
 }
